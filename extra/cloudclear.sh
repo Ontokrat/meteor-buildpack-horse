021c9796fbd6f104054e95c0a588fbebd3f5d3c1
Ontokrat <23409319+Ontokrat@users.noreply.github.com>
2018-02-14 17:58:59 -0800

    Purge CloudFlare#2

---
+++ extra/cloudclear.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Clear CloudFlare cache
+# Lauched on app deploy
+#
+
+
+purgeCache() {
+	local result=$(curl -L --write-out %{http_code} --silent --output /dev/null \
+	-X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache" \
+	-H "X-Auth-Email: ${CF_EMAIL}" \
+	-H "X-Auth-Key: ${CF_AUTH_KEY}" \
+	-H "Content-Type: application/json" \
+	--data '{"purge_everything":true}')
+	echo "$result"
+}
+
+if [ -n "\$CF_EMAIL" ] && [ -n "\$CF_AUTH_KEY" ] && [ -n "\$CF_ZONE_ID" ] ; then
+	echo "-----> Purging Cloudflare cache"
+
+	result=$(purgeCache)
+
+	if [ $result -ne "200" ]; then
+		echo "-----> Cache Purge Failed: ${result}"
+	else
+		echo "-----> Cache Purge Success: ${result}"
+	fi
+
+else
+	echo "-----> Purging Cloudflare Issue with config vars: check CF_ZONE_ID, CF_AUTH_KEY & CF_EMAIL"
+fi
+
+
